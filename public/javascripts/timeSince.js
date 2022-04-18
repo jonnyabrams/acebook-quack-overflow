8a6ba0d2338516535cd9fb6dcaf6b31c7f2fa0a6
James Mooney <jmooney9304@gmail.com>
2022-04-18 14:45:30 +0100

    Adding moment.js to project to improve date&time
    Co-authored-by: Valentina Maggio <valentina-maggio@users.noreply.github.com>

---
+++ public/javascripts/timeSince.js
@@ -0,0 +1,5 @@
+// const moment = require("moment");
+
+// document.querySelectorAll(".timeSince").forEach(date => {
+//   moment('2022-04-18T10:06:22.000+00:00', moment.defaultFormat).toDate()
+// });
