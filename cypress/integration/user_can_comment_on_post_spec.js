dbeeb89b53ca55c7d7ee5a27c52a51ea5b247e37
GDRMVRM <gdrmvrm@gmail.com>
2022-04-20 12:22:09 +0100

    users can comment on posts, implemented test

---
+++ cypress/integration/user_can_comment_on_post_spec.js
@@ -0,0 +1,17 @@
+const webHelper = require("../support/web_helpers.js");
+
+describe("Timeline", () => {
+  it("can submit comments on posts and view them", () => {
+    webHelper.signUp();
+    webHelper.signIn();
+    webHelper.submitPost();
+
+    // submit a comment
+
+    cy.get("#new-comment-form").find('[type="text"]').type("Awesome Cat!");
+    cy.get("#new-comment-form").submit();
+
+    cy.get("#comment").should("contain", "Awesome Cat!");
+    cy.get("#comment").should("contain", "a few seconds ago");
+  });
+});
