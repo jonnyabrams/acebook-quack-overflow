42ae8394027fed3c2ddc84ef57f1c84d7432df89
Valentina <maggio.vale@gmail.com>
2022-04-21 14:06:12 +0100

    Access to users attributes
    Co-authored-by: James Mooney <jam9304@users.noreply.github.com>

--- public/javascripts/friendsFeed.js
+++ public/javascripts/friendsFeed.js
@@ -1,15 +1,20 @@
 const myFriends = () => {
-  document.querySelectorAll(".posts").forEach(post => {
-    console.log("The post below:")
-    console.log(post);
-    
-    console.log("The Username below:")
-    console.log(post.getAttribute('username'));
-    // console.log('username');
-    console.log("we are here");
-    
-    // console.log(post.getAttribute("username"));
+  document.querySelectorAll(".username").forEach(username => {
+    console.log("The div user below:")
+    console.log(username);
+    console.log("The post user below:")
+    console.log(username.getAttribute("postUser"));
+    const postUser = username.getAttribute("postUser");
+    console.log("this is the const postUser");
+    console.log(postUser);
   });
+  const userSession = document.querySelector(".user-profile");
+  console.log("The session user below:")
+  console.log(userSession.getAttribute("sessionUser"));
+  const sessionUser = userSession.getAttribute("sessionUser");
+  console.log("this is the const sessionUser");
+  console.log(sessionUser);
 };
 
 myFriends();
+
