61e70116f78ce1e5182f9eafee3bd58b23bc47d0
Valentina <maggio.vale@gmail.com>
2022-04-21 17:16:02 +0100

    Creating filteredPost array
    Co-authored-by: James Mooney <jam9304@users.noreply.github.com>

--- public/javascripts/friendsFeed.js
+++ public/javascripts/friendsFeed.js
@@ -22,37 +22,34 @@ const myFriends = () => {
 
   // post users
   
-  document.querySelectorAll(".username").forEach(username => {
+  const filteredPost = [];
+
+  document.querySelectorAll(".posts").forEach(post => {
     console.log("The div user below:")
-    console.log(username);
+    console.log(post);
     console.log("The post user below:")
-    console.log(username.getAttribute("postUser"));
-    const postUser = username.getAttribute("postUser");
+    console.log(post.getAttribute("postUser"));
+    const postUser = post.getAttribute("postUser");
     console.log("this is the const postUser");
     console.log(postUser);
 
     // filtering friends
 
-    if (sessionFriendsList.includes(postUser)) {
+     if (sessionFriendsList.includes(postUser)) {
       console.log("this is filtered friends post");
-      console.log(postUser);
+      console.log(post);
+      filteredPost.push(post);
     } else {
       console.log("no friends");
+      
     }
-  });
 
-
-
-
-
-
-
-
-
-
-
+})
+console.log("printing filteredPost");
+console.log(filteredPost);
 
 };
 
 myFriends();
+
 
